9988b4d816fdd3ea75ad304c84f8a853a16cf6a1
dedelduwell <ardheliamputri@gmail.com>
2023-10-16 07:30:27 +0700

    Menambahkan nilai huruf, nilai setara, serta kualifikasi

--- PemilihanPercobaan205.java
+++ PemilihanPercobaan205.java
@@ -21,17 +21,26 @@ public class PemilihanPercobaan205 {
          if (total > 100 || total < 0 ) {
             grade = "Nilai Tidak Valid";
          } else if ( total > 80 ){
-            grade = "A";
+            grade = "A (4.0)";
+            grade += "(Sangat Baik)"; 
          } else if ( total > 73){
-            grade = "B+";
+            grade = "B+ (3.5)";
+            grade += "(Lebih dari Baik)";
          } else if ( total > 65){
-            grade = "C+";
+            grade = "B (3.0)";
+            grade += "Baik";
+         }else if ( total > 60){
+            grade = "C+ (2.5)";
+            grade += "Lebih dari Cukup";
          } else if ( total > 50){
-            grade = "C";
+            grade = "C (2.0)";
+            grade += "Cukup";
          } else if ( total > 39){
-            grade = "D";
+            grade = "D (1.0 )";
+            grade += "Kurang";
          } else if ( total < 39 ){
-            grade = "E";
+            grade = "E (0)";
+            grade += "Gagal";
          }
 
          System.out.println("Nilai akhir = " + total);
