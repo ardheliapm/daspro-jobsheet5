b4e6d0879192269b02635e442f0b80712c3b0980
dedelduwell <ardheliamputri@gmail.com>
2023-10-16 09:55:50 +0700

    Percobaan 3

---
+++ PemilihanPercobaan305.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+/**
+ * PemilihanPercobaan305
+ */
+public class PemilihanPercobaan305 {
+
+    public static void main(String[] args) {
+        
+        //Deklarasi Variabel
+        double angka1, angka2, hasil;
+        char operator;
+        
+        //Perintah Inputan
+        Scanner input05 = new Scanner(System.in);
+        System.out.println("Masukkan angka pertama");
+        angka1 = input05.nextDouble();
+        System.out.println("Masukkan Angka Kedua");
+        angka2 = input05.nextDouble();
+        System.out.println("Masukkan operator (+-*/): ");
+        operator = input05.next().charAt(0);
+        
+
+        switch (operator) 
+            {
+            case '+':
+                hasil = angka1 + angka2;
+                System.out.println(angka1 + " + " + angka2 + "=" + hasil);
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                System.out.println(angka1 + " - " + angka2 + "=" + hasil);
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                System.out.println(angka1 + " * " + angka2 + "=" + hasil);
+                break;
+            case '/':
+                hasil = angka1 / angka2;
+                System.out.println(angka1 + " / " + angka2 + "=" + hasil);
+                break;
+            }
+    }
+}
